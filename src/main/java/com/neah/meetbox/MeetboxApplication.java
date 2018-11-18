113e7ea33bfdeea08ac483afc5bb5b296be1080c
Thanapon Jaisanuk <thanapon.jsn@gmail.com>
2018-11-18 16:19:49 +0700

    init project

---
+++ src/main/java/com/neah/meetbox/MeetboxApplication.java
@@ -0,0 +1,12 @@
+package com.neah.meetbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetboxApplication.class, args);
+	}
+}
